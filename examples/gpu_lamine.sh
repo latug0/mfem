fa647aeab7334f0582daeca8a20e54e49b170006
Guillaume LATU <latuguil@topaze7002.c-topaze.mg1.ccrt.ccc.cea.fr>
2023-12-14 16:03:22 +0100

    Setup improved for tests on topaze

--- examples/gpu_lamine.sh
+++ examples/gpu_lamine.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+POSTPRO="-no-po"
+#POSTPRO=""
 (
-    CMD="time ./lamine -t 1 -o 2 -r 2 -no-po -m square_2mat_per.msh"
+    CMD="time ./lamine -t 1 -o 2 -r 3 $POSTPRO -m square_2mat_per.msh"
+    echo $CMD >&2; $CMD		    
+    CMD="time ./lamine -t 1 -o 2 -r 3 $POSTPRO -m square_2mat_per.msh -pa"
+    echo $CMD >&2; $CMD		    
+    CMD="time ./lamine -t 1 -o 2 -r 3 $POSTPRO -m square_2mat_per.msh -pa -d cuda"
     echo $CMD >&2; $CMD
-    CMD="time ./lamine -t 1 -o 2 -r 2 -no-po -m square_2mat_per.msh -pa"
-    echo $CMD >&2; $CMD
-    CMD="time ./lamine -t 1 -o 2 -r 1 -no-po -m square_2mat_per.msh -pa -d cuda"
-    echo $CMD >&2; $CMD
-    CMD="time ./lamine -t 3 -o 2 -r 1 -no-po -m cube_2mat_per.msh"
-    echo $CMD >&2; $CMD
-    CMD="time ./lamine -t 3 -o 2 -r 1 -no-po -m cube_2mat_per.msh -pa"
-    echo $CMD >&2; $CMD
-    CMD="time ./lamine -t 3 -o 2 -r 1 -no-po -m cube_2mat_per.msh -d cuda -pa"
+    CMD="time ./lamine -t 3 -o 2 -r 2 $POSTPRO -m cube_2mat_per.msh"
+    echo $CMD >&2; $CMD		    
+    CMD="time ./lamine -t 3 -o 2 -r 2 $POSTPRO -m cube_2mat_per.msh -pa"
+    echo $CMD >&2; $CMD		    
+    CMD="time ./lamine -t 3 -o 2 -r 2 $POSTPRO -m cube_2mat_per.msh -d cuda -pa"
     echo $CMD >&2; $CMD
 ) 1>out1 2>out2 
