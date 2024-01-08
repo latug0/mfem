bd37a90a580a7fb01d5ff8dce58e0fa79c8b9523
Guillaume LATU <latuguil@topaze174.c-topaze.mg1.ccrt.ccc.cea.fr>
2024-01-08 14:57:20 +0100

    prepare forall in order to do the assembly

--- fem/integ/bilininteg_elasticity_ea.cpp
+++ fem/integ/bilininteg_elasticity_ea.cpp
@@ -18,54 +18,56 @@ namespace mfem
 
 
 template<int T_D1D = 0, int T_Q1D = 0>
-static void EADiffusionAssemble2D(const int NE,
-                                  const Array<double> &b,
-                                  const Array<double> &g,
-                                  const Vector &padata,
-                                  Vector &eadata,
-                                  const bool add,
-                                  const int d1d = 0,
-                                  const int q1d = 0)
+static void EAElasticityAssemble2D(const int NE,
+				   const int NDOFS,
+				   const int NQ,
+				   const Array<double> &b,
+				   const Array<double> &g,
+				   const Vector &padata,
+				   Vector &eadata,
+				   const bool add,
+				   const int d1d = 0,
+				   const int q1d = 0)
 {
-   const int D1D = T_D1D ? T_D1D : d1d;
-   const int Q1D = T_Q1D ? T_Q1D : q1d;
-   MFEM_VERIFY(D1D <= MAX_D1D, "");
-   MFEM_VERIFY(Q1D <= MAX_Q1D, "");
-   auto B = Reshape(b.Read(), Q1D, D1D);
-   auto G = Reshape(g.Read(), Q1D, D1D);
-   auto D = Reshape(padata.Read(), Q1D, Q1D, 3, NE);
-   auto A = Reshape(eadata.ReadWrite(), D1D, D1D, D1D, D1D, NE);
-   mfem::forall_2D(NE, D1D, D1D, [=] MFEM_HOST_DEVICE (int e)
+//   const int D1D = T_D1D ? T_D1D : d1d;
+//   const int Q1D = T_Q1D ? T_Q1D : q1d;
+//   MFEM_VERIFY(D1D <= MAX_D1D, "");
+//   MFEM_VERIFY(Q1D <= MAX_Q1D, "");
+//   auto B = Reshape(b.Read(), Q1D, D1D);
+//   auto G = Reshape(g.Read(), Q1D, D1D);
+   constexpr int MDIM = 2;
+   auto LM = Reshape(padata.Read(), 2+MDIM*MDIM, NQ, NE);
+
+   // TODO: Check ndof value
+   auto A = Reshape(eadata.ReadWrite(), NDOFS, NDOFS, NE);
+   mfem::forall(NE*NDOFS, [=] MFEM_HOST_DEVICE (int e)
    {
-      const int D1D = T_D1D ? T_D1D : d1d;
-      const int Q1D = T_Q1D ? T_Q1D : q1d;
-      constexpr int MD1 = T_D1D ? T_D1D : MAX_D1D;
-      constexpr int MQ1 = T_Q1D ? T_Q1D : MAX_Q1D;
-      double r_B[MQ1][MD1];
-      double r_G[MQ1][MD1];
    });
 }
 
   
 template<int T_D1D = 0, int T_Q1D = 0>
-static void EADiffusionAssemble3D(const int NE,
-                                  const Array<double> &b,
-                                  const Array<double> &g,
-                                  const Vector &padata,
-                                  Vector &eadata,
-                                  const bool add,
-                                  const int d1d = 0,
-                                  const int q1d = 0)
+static void EAElasticityAssemble3D(const int NE,
+				   const int NDOFS,
+				   const int NQ,
+				   const Array<double> &b,
+				   const Array<double> &g,
+				   const Vector &padata,
+				   Vector &eadata,
+				   const bool add,
+				   const int d1d = 0,
+				   const int q1d = 0)
 {
-   const int D1D = T_D1D ? T_D1D : d1d;
-   const int Q1D = T_Q1D ? T_Q1D : q1d;
-   MFEM_VERIFY(D1D <= MAX_D1D, "");
-   MFEM_VERIFY(Q1D <= MAX_Q1D, "");
-   auto B = Reshape(b.Read(), Q1D, D1D);
-   auto G = Reshape(g.Read(), Q1D, D1D);
-   auto D = Reshape(padata.Read(), Q1D, Q1D, Q1D, 6, NE);
-   auto A = Reshape(eadata.ReadWrite(), D1D, D1D, D1D, D1D, D1D, D1D, NE);
-   mfem::forall_3D(NE, D1D, D1D, D1D, [=] MFEM_HOST_DEVICE (int e)
+//   const int D1D = T_D1D ? T_D1D : d1d;
+//   const int Q1D = T_Q1D ? T_Q1D : q1d;
+//   MFEM_VERIFY(D1D <= MAX_D1D, "");
+//   MFEM_VERIFY(Q1D <= MAX_Q1D, "");
+//   auto B = Reshape(b.Read(), Q1D, D1D);
+//   auto G = Reshape(g.Read(), Q1D, D1D);
+   constexpr int MDIM = 3;
+   auto LM = Reshape(padata.Read(), 2+MDIM*MDIM, NQ, NE);
+   auto A = Reshape(eadata.ReadWrite(), NDOFS, NDOFS, NE);
+   mfem::forall(NE*NDOFS, [=] MFEM_HOST_DEVICE (int e)
    {
       const int D1D = T_D1D ? T_D1D : d1d;
       const int Q1D = T_Q1D ? T_Q1D : q1d;
@@ -80,18 +82,18 @@ void ElasticityIntegrator::AssembleEA(const FiniteElementSpace &fes,
 				      const bool add)
 {
    AssemblePA(fes);
-   ne = fes.GetMesh()->GetNE();
+   //   ne = fes.GetMesh()->GetNE();
    const Array<double> &B = maps->B;
    const Array<double> &G = maps->G;
    if (dim == 2)
    {
-      return EADiffusionAssemble2D(ne,B,G,pa_data,ea_data,add,
-				   dofs1D,quad1D);
+     return EAElasticityAssemble2D(ne,ndof,nq,B,G,pa_data,ea_data,
+				   add,dofs1D,quad1D);
    }
    else if (dim == 3)
    {
-     return EADiffusionAssemble3D(ne,B,G,pa_data,ea_data,add,
-				  dofs1D,quad1D);
+     return EAElasticityAssemble3D(ne,ndof,nq,B,G,pa_data,ea_data,
+				   add,dofs1D,quad1D);
    }
    MFEM_ABORT("Unknown kernel.");
 }
